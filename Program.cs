88456edf00e1966761f24f016047e967285adc75
Gabriel Felix <gabrielfelix099@hotmail.com>
2026-02-20 16:56:36 -0300

    finalização do sistema de streaming

--- Program.cs
+++ Program.cs
@@ -11,7 +11,15 @@ Filme filme1 = new Filme
     Titulo = "Exorcista",
     AnoLancamento = 1970,
     ClassificacaoIndicativa = 18,
-    Diretor = "Miau"
+    Diretor = "Miau",
+};
+
+Filme filme2 = new Filme
+{
+    Titulo = "Superman",
+    AnoLancamento = 2024,
+    ClassificacaoIndicativa = 18,
+    Diretor = "Miau",
 };
 
 Musica musica1 = new Musica
@@ -21,6 +29,32 @@ Musica musica1 = new Musica
     AnoLancamento = 1984,
     ClassificacaoIndicativa = 16,
 };
+
+Musica musica2 = new Musica
+{
+    Titulo = "Hell on Earth",
+    Artista = "Iron Maiden",
+    AnoLancamento = 2021,
+    ClassificacaoIndicativa = 16,
+};
+
+Playlist playlist1 = new Playlist
+{
+    Nome = "Favoritos",
+    Conteudos = [],
+};
+
+playlist1.ArmazenarNaPlaylist(filme1);
+playlist1.ArmazenarNaPlaylist(musica1);
+playlist1.ArmazenarNaPlaylist(filme2);
+playlist1.ArmazenarNaPlaylist(musica2);
+
+playlist1.ImprimePlaylist();
+
+Console.WriteLine("\nFilmes e músicas lançados depois de 2020: \n");
+playlist1.ImprimeLancamentoRecente();
+
+Console.WriteLine();
 
 Console.WriteLine(usuario.PodeReproduzir(musica1));
 
--- models/Playlist.cs
+++ models/Playlist.cs
@@ -3,6 +3,42 @@
 public class Playlist
 {
     public string Nome { get; set; }
-    public List<Conteudo> conteudos { get; set; } = [];
+    public List<Conteudo> Conteudos { get; set; }
+
+    public void ArmazenarNaPlaylist(Conteudo conteudo)
+    {
+        Conteudos.Add(conteudo);
+    }
+
+    public string ImprimeNomePlaylis()
+    {
+        return $"Titulo da playlist - {Nome}";
+    }
+
+    public void ImprimePlaylist()
+    {
+        Console.WriteLine($"{ImprimeNomePlaylis()}: \n");
+
+        foreach (Conteudo conteudo in Conteudos)
+        {
+            if (conteudo is Musica musica)
+                Console.WriteLine($"{musica} - Titulo: {musica.Titulo} | Ano lançamento: {musica.AnoLancamento} | Classificação Indicativa: {musica.ClassificacaoIndicativa} | Artista: {musica.Artista}");
+
+            if (conteudo is Filme filme)
+                Console.WriteLine($"{filme} - Titulo: {filme.Titulo} | Ano lançamento: {filme.AnoLancamento} | Classificação Indicativa: {filme.ClassificacaoIndicativa} | Diretor: {filme.Diretor}");
+        }
+    }
+
+    public void ImprimeLancamentoRecente()
+    {
+        foreach (Conteudo conteudo in Conteudos)
+        {
+            if (conteudo is Musica musica && conteudo.AnoLancamento > 2020)
+                Console.WriteLine($"{musica} - Titulo: {musica.Titulo} | Ano lançamento: {musica.AnoLancamento} | Classificação Indicativa: {musica.ClassificacaoIndicativa} | Artista: {musica.Artista}");
+
+            if (conteudo is Filme filme && conteudo.AnoLancamento > 2020)
+                Console.WriteLine($"{filme} - Titulo: {filme.Titulo} | Ano lançamento: {filme.AnoLancamento} | Classificação Indicativa: {filme.ClassificacaoIndicativa} | Diretor: {filme.Diretor}");
+        }
+    }
 
 }
